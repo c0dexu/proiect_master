10f8290554df0b2243a905a8c291b288d6ff1872
Topan Sergiu <sergiu.smw@gmail.com>
2023-06-08 20:45:41 +0300

    updated constants

--- constants.js
+++ constants.js
@@ -1,5 +1,4 @@
-export const UNIT_EARTH = 16; // 1 earth radius
-export const UNIT_LENGTH = UNIT_EARTH / 6371;
-export const UNIT_MASS = 0.25; // 1g = 0.25u
-export const UNIT_EARTH_MASS = UNIT_MASS * 1000 * 5.97e24;
-export const G_ACC = 0.045; // 9.8 m/s^-2 = 0.045
+export const UNIT_AU = 512; // 1 AU = 512px
+export const UNIt_RADIUS = 16; // 1 earth radius
+export const UNIT_MASS = 0.0024; // 1 earth mass
+export const G_CONST = 0.00002;
